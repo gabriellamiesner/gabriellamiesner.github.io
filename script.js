eeb3e05c2ae9ab0acfdd3576305c5b1b7145ba86
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-03 14:50:01 +0000

    filtration system how i want it

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function showDiv() {
     div = document.getElementById('by_semester');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
     }
 }
 function showDiv1() {
     div = document.getElementById('by_subject');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
-      document.getElementById('by_sem').style.display = "inline";
+      document.getElementById('by_sem').style.display = "block";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
     document.getElementById('by_sem').style.display = "none";
     }
 }
 
 function showKodeWithKlossy() {
     div = document.getElementById('kwk');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
     document.getElementById('pickhacks').style.display = "none";
     document.getElementById('techtogether').style.display = "none";
     document.getElementById('google').style.display = "none";
@@ -33,10 +33,10 @@ function showKodeWithKlossy() {
 
 function showTechTogether() {
     div = document.getElementById('techtogether');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
   document.getElementById('kwk').style.display = "none";
     document.getElementById('pickhacks').style.display = "none";
     document.getElementById('google').style.display = "none";
@@ -48,10 +48,10 @@ function showTechTogether() {
 
 function showLaunchCode() {
     div = document.getElementById('launchcode');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
    document.getElementById('kwk').style.display = "none";
     document.getElementById('techtogether').style.display = "none";
     document.getElementById('google').style.display = "none";
@@ -61,10 +61,10 @@ function showLaunchCode() {
 
 function showGoogle() {
     div = document.getElementById('google');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
     document.getElementById('kwk').style.display = "none";
     document.getElementById('techtogether').style.display = "none";
     document.getElementById('pickhacks').style.display = "none";
@@ -74,10 +74,10 @@ function showGoogle() {
 
 function showPickHacks() {
     div = document.getElementById('pickhacks');
-    if (div.style.display == "inline"){
+    if (div.style.display == "block"){
       div.style.display = "none";
     }else{
-    div.style.display = "inline";
+    div.style.display = "block";
     document.getElementById('kwk').style.display = "none";
     document.getElementById('techtogether').style.display = "none";
     document.getElementById('google').style.display = "none";
@@ -85,7 +85,7 @@ function showPickHacks() {
     }
 }
 
-filterSelection("all");
+
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
