fb14e4954192c0cbd41df043a009ca1e44c051c9
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-03 01:20:43 +0000

    classes page is working!!

---
+++ script.js
@@ -0,0 +1,16 @@
+function showDiv() {
+    div = document.getElementById('by_semester');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+    }
+}
+function showDiv1() {
+    div = document.getElementById('by_subject');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+    }
+}
