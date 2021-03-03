24b2439986e61c0acb4735969705c87edfa20c16
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-03 02:43:27 +0000

    orgs on about page

--- script.js
+++ script.js
@@ -16,3 +16,71 @@ function showDiv1() {
     document.getElementById('by_sem').style.display = "none";
     }
 }
+
+function showKodeWithKlossy() {
+    div = document.getElementById('kwk');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+    document.getElementById('pickhacks').style.display = "none";
+    document.getElementById('techtogether').style.display = "none";
+    document.getElementById('google').style.display = "none";
+    document.getElementById('launchcode').style.display = "none";
+    }
+}
+
+
+function showTechTogether() {
+    div = document.getElementById('techtogether');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+  document.getElementById('kwk').style.display = "none";
+    document.getElementById('pickhacks').style.display = "none";
+    document.getElementById('google').style.display = "none";
+    document.getElementById('launchcode').style.display = "none";
+    
+    
+    }
+}
+
+function showLaunchCode() {
+    div = document.getElementById('launchcode');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+   document.getElementById('kwk').style.display = "none";
+    document.getElementById('techtogether').style.display = "none";
+    document.getElementById('google').style.display = "none";
+    document.getElementById('pickhacks').style.display = "none";
+    }
+}
+
+function showGoogle() {
+    div = document.getElementById('google');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+    document.getElementById('kwk').style.display = "none";
+    document.getElementById('techtogether').style.display = "none";
+    document.getElementById('pickhacks').style.display = "none";
+    document.getElementById('launchcode').style.display = "none";
+    }
+}
+
+function showPickHacks() {
+    div = document.getElementById('pickhacks');
+    if (div.style.display == "block"){
+      div.style.display = "none";
+    }else{
+    div.style.display = "block";
+    document.getElementById('kwk').style.display = "none";
+    document.getElementById('techtogether').style.display = "none";
+    document.getElementById('google').style.display = "none";
+    document.getElementById('launchcode').style.display = "none";
+    }
+}
