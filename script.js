7c45d3f64c085e7e54c021d6904800247d074ab8
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-03 01:28:21 +0000

    improving classes page design

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ function showDiv1() {
     div = document.getElementById('by_subject');
     if (div.style.display == "block"){
       div.style.display = "none";
+      document.getElementById('by_sem').style.display = "block";
     }else{
     div.style.display = "block";
+    document.getElementById('by_sem').style.display = "none";
     }
 }
