e9f1382fa12742d16468b7f22e488a2ade342d69
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-04 22:14:39 +0000

    fixing font?

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function w3RemoveClass(element, name) {
 function changeSize(){
    let b = document.querySelector('#about-text')
    if(window.innerWidth < 600){
-      b.style.fontSize = '.75rem'
+      b.style.fontSize = '1rem'
   } else{
       b.style.fontSize = '1.25rem'
   }
